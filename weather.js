fc4c8263b5b7553e7005d9685df8db05ff58fffc
Jaykumar Patel <jpate238@uic.edu>
2019-12-03 17:55:27 +0000

    Card display for weather

--- weather.js
+++ weather.js
@@ -1,15 +1,19 @@
-endpoint = "https://cors-anywhere.herokuapp.com/https://api.weatherbit.io/v2.0/current?key=2f20bc29ce0f4b25a2a7a8474f91a6ea";
+endpoint = "https://api.weatherbit.io/v2.0/current?key=b6c58d04639142ddbe58fee338f6c6bd";
 // console.log(endpoint);
 // var hey = 1;
 
+console.log("Here");
 $(document).ready( function(){
     $("#wBtn").on("click", function(){
-//     console.log($('#city').val() + $('#state').val() + $('#country').val() );
     $.get(endpoint + "&country="+ $('#country').val() + "&state=" +$('#state').val() + "&city=" +$('#city').val(), function(response){
          $.each(response, function(i,v) {
           if(v != 1){
              console.log(v[0].temp);
+             var data= "<div class= 'card'>" + "<li class='list-group-item'>" + v[0].city_name + ", " + v[0].state_code + "<br>" +
+                 "~ "+v[0].weather.description +" ~"+ "<br>" + "Current Tempertature: " + v[0].temp+ " °C"+ "<br>" + "Wind Speed: " + v[0].wind_spd  + "<br>" + "</li>" + "<br>" + "</div>" ;
+              $("#list").append(data);
           }
+             
          });
         
         
--- wiki.js
+++ wiki.js
@@ -1,9 +1,9 @@
 var w_endpoint = "https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&search=";
 
 $(document).ready( function(){
-    $("#wBtn").on("click", function(){
-    console.log($('#wInput').val());
-    $.get(w_endpoint + $('#wInput').val(), function(response){
+    $("#btn").on("click", function(){
+    console.log($('#input').val());
+    $.get(w_endpoint + $('#input').val(), function(response){
         console.log(response);
      
       });
